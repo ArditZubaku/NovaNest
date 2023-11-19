48f59bc1775ec082ca4d2e3c7e05b81ebfc5812c
Ardit Zubaku <zubaku92@gmail.com>
2023-11-19 04:14:24 +0100

    Added Login FXML & CSS files and created the corresponding controller properties.

--- src/main/java/com/zubaku/novanest/App.java
+++ src/main/java/com/zubaku/novanest/App.java
@@ -1,11 +1,21 @@
 package com.zubaku.novanest;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class App extends Application {
   @Override
-  public void start(Stage stage) {}
+  public void start(Stage stage) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
+    Scene scene = new Scene(fxmlLoader.load());
+    stage.setScene(scene);
+    stage.setTitle("NovaNest ");
+    stage.show();
+  }
 
   public static void main(String[] args) {
     launch();
--- src/main/java/com/zubaku/novanest/controllers/LoginController.java
+++ src/main/java/com/zubaku/novanest/controllers/LoginController.java
@@ -1,3 +1,13 @@
 package com.zubaku.novanest.controllers;
 
-public class LoginController {}
+import javafx.scene.control.*;
+
+public class LoginController {
+  public ChoiceBox accountSelector;
+  public Label payeeAddressLabel;
+  public TextField payeeAddressField;
+  public Label passwordLabel;
+  public PasswordField passwordField;
+  public Button loginButton;
+  public Label errorLabel;
+}
