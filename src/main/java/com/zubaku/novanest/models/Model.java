59a119fd8292ebe87745f65baafec5bc9166323f
Ardit Zubaku <zubaku92@gmail.com>
2023-11-19 23:05:38 +0100

    Implemented thread-safe of the Singleton pattern using the initialization-on-demand holder idiom.

--- src/main/java/com/zubaku/novanest/models/Model.java
+++ src/main/java/com/zubaku/novanest/models/Model.java
@@ -1,3 +1,26 @@
 package com.zubaku.novanest.models;
 
-public class Model {}
+import com.zubaku.novanest.views.ViewProcessor;
+
+public class Model {
+  private final ViewProcessor viewProcessor;
+
+  // Private constructor.
+  private Model() {
+    this.viewProcessor = new ViewProcessor();
+  }
+
+  // Static inner class for holding the instance.
+  private static class Holder {
+    private static final Model INSTANCE = new Model();
+  }
+
+  // Public method to get the instance.
+  public static Model getInstance() {
+    return Holder.INSTANCE;
+  }
+
+  public ViewProcessor getViewProcessor() {
+    return viewProcessor;
+  }
+}
