458da0d52eb6b6b90eb0ff2f94d8a983112d6d7f
Ardit Zubaku <zubaku92@gmail.com>
2023-11-19 23:12:35 +0100

    Added ViewProcessor for creating stages and showing windows.

--- src/main/java/com/zubaku/novanest/views/ViewFactory.java
+++
@@ -1,3 +0,0 @@
-package com.zubaku.novanest.views;
-
-public class ViewFactory {}
---
+++ src/main/java/com/zubaku/novanest/views/ViewProcessor.java
@@ -0,0 +1,55 @@
+package com.zubaku.novanest.views;
+
+import com.zubaku.novanest.controllers.client.ClientController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+public class ViewProcessor {
+  private static final Logger LOGGER = Logger.getLogger(ViewProcessor.class.getName());
+  // Client Views
+  private AnchorPane dashboardView;
+
+  public ViewProcessor() {}
+
+  public AnchorPane getDashboardView() {
+    if (dashboardView == null) {
+      try {
+        dashboardView =
+            new FXMLLoader(getClass().getResource("/fxml/client/Dashboard.fxml")).load();
+      } catch (Exception e) {
+        LOGGER.log(Level.SEVERE, "Error loading dashboard view", e);
+      }
+    }
+    return dashboardView;
+  }
+
+  public void showLoginWindow() {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
+    createStage(loader);
+  }
+
+  public void showClientWindow() {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/client/Client.fxml"));
+    ClientController clientController = new ClientController();
+    loader.setController(clientController);
+    createStage(loader);
+  }
+
+  private void createStage(FXMLLoader loader) {
+    Scene scene = null;
+    try {
+      scene = new Scene(loader.load());
+    } catch (Exception e) {
+      LOGGER.log(Level.SEVERE, "Error creating stage", e);
+    }
+    Stage stage = new Stage();
+    stage.setScene(scene);
+    stage.setTitle("NovaNest Bank");
+    stage.show();
+  }
+}
