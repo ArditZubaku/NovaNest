62f3ae7cb6dce3113d2b39747b2bf3de2988d7aa
Ardit Zubaku <zubaku92@gmail.com>
2023-11-20 17:02:19 +0100

    TODO: Add switch statement in the AdminController to update the parent view based on the buttons that are clicked/selected.

--- src/main/java/com/zubaku/novanest/controllers/admin/AdminController.java
+++ src/main/java/com/zubaku/novanest/controllers/admin/AdminController.java
@@ -1,3 +1,23 @@
 package com.zubaku.novanest.controllers.admin;
 
-public class AdminController {}
+import com.zubaku.novanest.models.Model;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminController implements Initializable {
+  public BorderPane adminParent;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    Model.getInstance()
+        .getViewProcessor()
+        .getAdminSelectedMenuItem()
+        .addListener(
+            (observable, oldValue, newValue) -> {
+              // TODO: Add switch statement
+            });
+  }
+}
