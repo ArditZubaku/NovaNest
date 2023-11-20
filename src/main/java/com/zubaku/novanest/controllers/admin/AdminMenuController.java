979f3037e20651eb5868af8f7d7707b68e1f1988
Ardit Zubaku <zubaku92@gmail.com>
2023-11-20 12:58:52 +0100

    Added AdminMenu FXML & Controller, used the already existing clientMenu.css since it is similar.

---
+++ src/main/java/com/zubaku/novanest/controllers/admin/AdminMenuController.java
@@ -0,0 +1,17 @@
+package com.zubaku.novanest.controllers.admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminMenuController implements Initializable {
+  public Button createClientButton;
+  public Button clientsButton;
+  public Button depositButton;
+  public Button logoutButton;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {}
+}
