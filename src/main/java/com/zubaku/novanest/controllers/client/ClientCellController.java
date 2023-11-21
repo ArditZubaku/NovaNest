1bd1a0b7b6ecf885697d7540405b087e1a7f0ffa
Ardit Zubaku <zubaku92@gmail.com>
2023-11-22 00:20:59 +0100

    Added ClientCell FXML & CSS files and also the corresponding fields in its controller.

---
+++ src/main/java/com/zubaku/novanest/controllers/client/ClientCellController.java
@@ -0,0 +1,23 @@
+package com.zubaku.novanest.controllers.client;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientCellController implements Initializable {
+  public Label dateLabel;
+  public Button deleteButton;
+  public Label savingsAccountLabel;
+  public Label checkingAccountLabel;
+  public Label pAddressLabel;
+  public Label lastNameLabel;
+  public Label firstNameLabel;
+
+  @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
