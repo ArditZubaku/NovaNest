595b86c327175fe562f52aa4bc2fd3571de399e3
Ardit Zubaku <zubaku92@gmail.com>
2023-11-22 23:40:27 +0100

    Create account for client.

--- src/main/java/com/zubaku/novanest/controllers/admin/CreateClientController.java
+++ src/main/java/com/zubaku/novanest/controllers/admin/CreateClientController.java
@@ -1,13 +1,17 @@
 package com.zubaku.novanest.controllers.admin;
 
+import static com.zubaku.novanest.utils.constants.Constants.TRANSACTION_LIMIT_PER_DAY;
+import static com.zubaku.novanest.utils.constants.Constants.WITHDRAWAL_LIMIT;
+
+import com.zubaku.novanest.models.Model;
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class CreateClientController implements Initializable {
   public TextField firstNameTextField;
@@ -22,8 +26,27 @@ public class CreateClientController implements Initializable {
   public Button createNewClientButton;
   public Label errorLabel;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+  private String payeeAddress;
 
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {}
+
+  private void createAccount(String accountType) {
+    double balance = Double.parseDouble(checkingAccountBalanceAmountTextField.getText());
+    // Generate account number
+    String firstPart = "3201";
+    String lastPart = Integer.toString(new Random().nextInt(999) + 1000);
+    String accountNumber = firstPart + " " + lastPart;
+
+    // Create the checking or savings account
+    if (accountType.equals("Checking")) {
+      Model.getInstance()
+          .getRepository()
+          .createCheckingAccount(payeeAddress, accountNumber, TRANSACTION_LIMIT_PER_DAY, balance);
+    } else {
+      Model.getInstance()
+          .getRepository()
+          .createSavingsAccount(payeeAddress, accountNumber, WITHDRAWAL_LIMIT, balance);
     }
+  }
 }
---
+++ src/main/java/com/zubaku/novanest/utils/constants/Constants.java
@@ -0,0 +1,6 @@
+package com.zubaku.novanest.utils.constants;
+
+public class Constants {
+  public static final int TRANSACTION_LIMIT_PER_DAY = 10;
+  public static final double WITHDRAWAL_LIMIT = 2000;
+}
