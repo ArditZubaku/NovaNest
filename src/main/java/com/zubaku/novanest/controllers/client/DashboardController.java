ddde811ca3599174f9d1b58e955b43a4e73300b4
Ardit Zubaku <zubaku92@gmail.com>
2023-11-23 21:15:02 +0100

    Binding the data on Dashboard as soon as user logs in.

--- src/main/java/com/zubaku/novanest/controllers/client/DashboardController.java
+++ src/main/java/com/zubaku/novanest/controllers/client/DashboardController.java
@@ -1,7 +1,12 @@
 package com.zubaku.novanest.controllers.client;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+
+import com.zubaku.novanest.models.Model;
+import com.zubaku.novanest.models.Transaction;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
@@ -15,7 +20,7 @@ public class DashboardController implements Initializable {
   public Label savingsAccountNumber;
   public Text incomeLabel;
   public Text expensesLabel;
-  public ListView transactionsListView;
+  public ListView<Transaction> transactionsListView;
   public TextField payeeTextField;
   public TextField amountTextField;
   public TextArea messageTextArea;
@@ -23,6 +28,44 @@ public class DashboardController implements Initializable {
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
+    bindData();
+  }
 
+  private void bindData() {
+    usernameText
+        .textProperty()
+        .bind(Bindings.concat("Hi, ").concat(Model.getInstance().getClient().firstNameProperty()));
+
+    loginDate.setText("Today, " + LocalDate.now());
+    checkingBalance
+        .textProperty()
+        .bind(
+            Model.getInstance()
+                .getClient()
+                .checkingAccountProperty()
+                .get()
+                .balanceProperty()
+                .asString());
+    checkingAccountNumber
+        .textProperty()
+        .bind(
+            Model.getInstance()
+                .getClient()
+                .checkingAccountProperty()
+                .get()
+                .accountNumberProperty());
+    savingsBalance
+        .textProperty()
+        .bind(
+            Model.getInstance()
+                .getClient()
+                .savingsAccountProperty()
+                .get()
+                .balanceProperty()
+                .asString());
+    savingsAccountNumber
+        .textProperty()
+        .bind(
+            Model.getInstance().getClient().savingsAccountProperty().get().accountNumberProperty());
   }
 }
